81ad1ac069f9a1e27b6404047ef6547a7b879c12
ionilancer <ionilancer@gmail.com>
2020-11-11 11:39:04 +0100

    primer commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDfO8K2FLkQME9xrvvotdFdyYr2INSuiFQ',
-  authDomain: 'birthday-d91d7.firebaseapp.com',
-  databaseURL: 'https://birthday-d91d7.firebaseio.com',
-  projectId: 'birthday-d91d7',
-  storageBucket: 'birthday-d91d7.appspot.com',
-  messagingSenderId: '669007744705',
-  appId: '1:669007744705:web:fdb696c311e01d8948ce40',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 export default firebase.initializeApp(firebaseConfig);
